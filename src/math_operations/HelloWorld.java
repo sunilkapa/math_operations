1753bf188cb2ae847c681809f5490119bb9a8ebe
Sunil Kapa <kapasunil@gmail.com>
2020-05-27 21:48:15 +0530

    Addition method

--- src/math_operations/HelloWorld.java
+++ src/math_operations/HelloWorld.java
@@ -7,4 +7,10 @@ public class HelloWorld
 		System.out.println("Hello World");
 		System.out.println("Hello");
 	}
+	
+	public void add(int a, int b)
+	{
+		int c = a + b;
+		System.out.println("Sum Of numbers : " + c);
+	}
 }
